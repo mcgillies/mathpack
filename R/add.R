66eb2cbedf2c5de35a07ac62cfbf05a580353726
Matthew Gillies <mcgillies@shaw.ca>
2023-04-18 13:59:10 -0700

    updated add documentation

--- R/add.R
+++ R/add.R
@@ -1,3 +1,14 @@
+#' Adds two supplied numbers
+#'
+#' @param x An integer or float
+#' @param y An integer or float
+#'
+#' @return An integer or float that is the sum of the two supplied parameters
+#' @export
+#'
+#' @examples
+#' add(1,2)
+#' add(10.65, 20.78)
 add <- function(x,y) {
   return(x+y)
 }
