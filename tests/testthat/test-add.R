65821e31edc5d272ac6c338ccffe47e9552b8cc0
Matthew Gillies <mcgillies@shaw.ca>
2023-04-18 14:07:03 -0700

    added commas after test description

--- tests/testthat/test-add.R
+++ tests/testthat/test-add.R
@@ -2,6 +2,6 @@ test_that("adds correctly", {
   expect_equal(add(2,2), 4)
 })
 
-test_that("adds floats correctly" {
+test_that("adds floats correctly", {
   expect_equal(add(10.5, 1.2), 11.7)
 })
--- tests/testthat/test-subtract.R
+++ tests/testthat/test-subtract.R
@@ -1,9 +1,9 @@
 test_that("subtracts correctly", {
   expect_equal(subtract(9,4), 5)
 })
-test_that("subtracts floats correctly" {
+test_that("subtracts floats correctly", {
   expect_equal(subtract(9.5,4.5), 5)
 })
-test_that("returns negatives" {
+test_that("returns negatives", {
   expect_equal(subtract(1,2), -1)
 })
