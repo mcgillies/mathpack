7d56f29d6bed5266432470930916a7ff50d26c8a
Matthew Gillies <mcgillies@shaw.ca>
2023-04-18 14:00:24 -0700

    documented subtract

--- R/subtract.R
+++ R/subtract.R
@@ -1,3 +1,14 @@
+#' Subtracts the second parameter from the first
+#'
+#' @param x An integer or float
+#' @param y An integer or float
+#'
+#' @return The difference between x and y, and integer or float
+#' @export
+#'
+#' @examples
+#' subtract(1,2)
+#' subtract(10.87, 1.111)
 subtract <- function(x,y) {
   return(x-y)
 }
